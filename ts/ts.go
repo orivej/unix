5bc14f405390f42a989383ebc0a00602fd2a496e
Orivej Desh <orivej@gmx.fr>
2017-01-30 13:30:34 +0000

    Do not change line endings

--- ts/ts.go
+++ ts/ts.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"time"
@@ -11,17 +12,28 @@ func main() {
 	startup := time.Now()
 	previous := startup
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(ScanLines)
 	for scanner.Scan() {
 		instant := time.Now()
 		fmt.Printf(
-			"%7.3f %7.3f   %v\n",
+			"%7.3f %7.3f   %s",
 			instant.Sub(previous).Seconds(),
 			instant.Sub(startup).Seconds(),
-			scanner.Text(),
+			scanner.Bytes(),
 		)
 		previous = instant
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "input error:", err)
 	}
+}
+
+func ScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) > 0 {
+		return len(data), data, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		return i + 1, data[:i+1], nil
+	}
+	return 0, nil, nil
 }
