291392dba16b464fed801353ac01a3776d145b69
Orivej Desh <orivej@gmx.fr>
2019-09-04 02:33:31 +0000

    Add floatbits tool to examine IEEE 754 floats

---
+++ floatbits/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+	"unsafe"
+)
+
+var exactlyString = map[bool]string{false: "inexactly", true: "exactly"}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Arguments: NUMBER")
+		os.Exit(1)
+	}
+	r, ok := new(big.Rat).SetString(os.Args[1])
+	if !ok {
+		fmt.Println("Failed to parse NUMBER:", os.Args[1])
+		os.Exit(1)
+	}
+	fmt.Println("input:", r.RatString())
+
+	f64, e64 := r.Float64()
+	fmt.Println("float64:", new(big.Rat).SetFloat64(f64))
+	fmt.Println("float64: ", f64, exactlyString[e64])
+	f64a, f64b := aroundFloat64(f64)
+	fmt.Println("float64-:", f64a)
+	fmt.Println("float64+:", f64b)
+
+	f32, e32 := r.Float32()
+	fmt.Println("float32:", new(big.Rat).SetFloat64(float64(f32)))
+	fmt.Println("float32: ", f32, exactlyString[e32])
+	f32a, f32b := aroundFloat32(f32)
+	fmt.Println("float32-:", f32a)
+	fmt.Println("float32+:", f32b)
+
+	fmt.Println("32 as 64:", float64(f32))
+}
+
+func float32uint(x float32) uint32 { return *(*uint32)(unsafe.Pointer(&x)) }
+func float64uint(x float64) uint64 { return *(*uint64)(unsafe.Pointer(&x)) }
+func uint32float(x uint32) float32 { return *(*float32)(unsafe.Pointer(&x)) }
+func uint64float(x uint64) float64 { return *(*float64)(unsafe.Pointer(&x)) }
+
+func aroundFloat32(x float32) (float32, float32) {
+	xi := float32uint(x)
+	return uint32float(xi - 1), uint32float(xi + 1)
+}
+
+func aroundFloat64(x float64) (float64, float64) {
+	xi := float64uint(x)
+	return uint64float(xi - 1), uint64float(xi + 1)
+}
